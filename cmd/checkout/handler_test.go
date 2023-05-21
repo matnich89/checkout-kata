c0f49b6e55780c685be0eee499dad824770737ed
Mathew Nicholls <mathew.nicholls@yandex.com>
2023-05-21 19:10:11 +0100

    Initial Test for Total http handler

--- cmd/checkout/handler_test.go
+++ cmd/checkout/handler_test.go
@@ -35,3 +35,20 @@ func TestScan(t *testing.T) {
 		require.Equal(t, http.StatusBadRequest, resp.StatusCode)
 	})
 }
+
+func TestTotal(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	m := mockcheckout.NewMockCheckout(ctrl)
+	app := newTestApplication(t, m)
+	server := newTestServer(t, app.routes())
+	defer server.Close()
+
+	t.Run("should return 200", func(t *testing.T) {
+		req, _ := http.NewRequest(http.MethodGet, server.URL+"/checkout/total", nil)
+		resp, err := server.Client().Do(req)
+		require.NoError(t, err)
+		require.Equal(t, http.StatusOK, resp.StatusCode)
+	})
+
+}
