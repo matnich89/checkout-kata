d2f56eaefa6bdcbc2b4b3f393537d2b9d272a8ae
Mathew Nicholls <matnicholls1989@aol.co.uk>
2023-05-21 12:27:11 +0100

    Implemented scan

--- internal/checkout/checkout.go
+++ internal/checkout/checkout.go
@@ -1,6 +1,9 @@
 package checkout
 
-import "checkoutkata/internal/model"
+import (
+	"checkoutkata/internal/model"
+	"fmt"
+)
 
 type Checkout interface {
 	Scan(itemSku string) error
@@ -8,16 +11,32 @@ type Checkout interface {
 }
 
 type StandardCheckout struct {
-	currentScannedItems []model.Item
-	availableItems      map[string]model.Item
+	currentScannedItems map[string]int
+	availableItems      map[string]*model.Item
 }
 
-func NewStandardCheckout() *StandardCheckout {
-	return &StandardCheckout{}
+func NewStandardCheckout(initialItems []model.Item) *StandardCheckout {
+
+	availableItems := map[string]*model.Item{}
+
+	for _, it := range initialItems {
+		availableItems[it.SKU] = &it
+	}
+
+	return &StandardCheckout{
+		currentScannedItems: map[string]int{},
+		availableItems:      availableItems,
+	}
 }
 
 func (c *StandardCheckout) Scan(itemSku string) error {
-	return nil
+
+	if _, ok := c.availableItems[itemSku]; ok {
+		c.currentScannedItems[itemSku] = c.currentScannedItems[itemSku] + 1
+		return nil
+	}
+
+	return fmt.Errorf("invalid item SKU: %s", itemSku)
 }
 
 func (c *StandardCheckout) GetTotalPrice() int {
--- internal/checkout/checkout_test.go
+++ internal/checkout/checkout_test.go
@@ -1,22 +1,61 @@
 package checkout
 
 import (
+	"checkoutkata/internal/model"
 	"github.com/stretchr/testify/require"
 	"testing"
 )
 
 func TestScan(t *testing.T) {
-	checkout := NewStandardCheckout()
+	checkout := NewStandardCheckout(setupItems())
 
-	err := checkout.Scan("A")
+	t.Run("should scan available item", func(t *testing.T) {
+		err := checkout.Scan("A")
+		require.NoError(t, err)
+	})
 
-	require.NoError(t, err)
+	t.Run("should handle invalid item", func(t *testing.T) {
+		err := checkout.Scan("Z")
+		require.Error(t, err)
+	})
 }
 
 func TestGetTotal(t *testing.T) {
-	checkout := NewStandardCheckout()
+	checkout := NewStandardCheckout(setupItems())
 
 	total := checkout.GetTotalPrice()
 
 	require.Equal(t, 0, total)
+}
+
+func setupItems() []model.Item {
+	itemA := model.Item{
+		SKU:       "A",
+		UnitPrice: 50,
+		SpecialPrice: &model.SpecialPrice{
+			AmountRequired: 3,
+			Price:          130,
+		},
+	}
+
+	itemB := model.Item{
+		SKU:       "B",
+		UnitPrice: 30,
+		SpecialPrice: &model.SpecialPrice{
+			AmountRequired: 2,
+			Price:          45,
+		},
+	}
+
+	itemC := model.Item{
+		SKU:       "C",
+		UnitPrice: 20,
+	}
+
+	itemD := model.Item{
+		SKU:       "D",
+		UnitPrice: 15,
+	}
+
+	return []model.Item{itemA, itemB, itemC, itemD}
 }
